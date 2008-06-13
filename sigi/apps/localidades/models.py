98bd4a7cf948db31903e6dd7b994b3e510fd7313
Guilherme Gondim <guilhermegondim>
2008-06-13 19:44:56 +0000

    Adicionado novos campos utilizados pelo IBGE.

--- sigi/apps/localidades/models.py
+++ sigi/apps/localidades/models.py
@@ -2,6 +2,13 @@
 from django.db import models
 
 class UnidadeFederativa(models.Model):
+    REGIAO_CHOICES = (
+        ('SL', 'Sul'),
+        ('SD', 'Sudeste'),
+        ('CO', 'Centro-Oeste'),
+        ('NE', 'Nordeste'),
+        ('NO', 'Norte'),
+    )
     codigo_ibge = models.PositiveIntegerField(
         u'código IBGE',
         primary_key=True,
@@ -9,6 +16,8 @@ class UnidadeFederativa(models.Model):
     )
     nome = models.CharField(max_length=25)
     sigla = models.CharField(max_length=2, help_text="Exemplo: <em>MG</em>.")
+    regiao = models.CharField(max_length=2, choices=REGIAO_CHOICES)
+    populacao = models.PositiveIntegerField('população')
 
     class Meta:
         ordering = ('nome',)
@@ -29,8 +38,17 @@ class Municipio(models.Model):
         primary_key=True,
         help_text='Código do município segundo IBGE.'
     )
+    codigo_mesorregiao = models.PositiveIntegerField(
+        u'código mesorregião',
+    )
+    codigo_microrregiao = models.PositiveIntegerField(
+        u'código microrregião',
+    )
     nome = models.CharField(max_length=50)
     uf = models.ForeignKey(UnidadeFederativa, verbose_name='UF')
+    is_capital = models.BooleanField('capital')
+    populacao = models.PositiveIntegerField(u'população')
+    is_polo = models.BooleanField(u'pólo')
     latitude = models.DecimalField(
         max_digits=10,
         decimal_places=8,
