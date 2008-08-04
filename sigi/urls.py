d47db625efaca5c6828d93c5babb0adede11c215
Guilherme Gondim <guilhermegondim>
2008-08-04 21:19:51 +0000

    Corrigo urls.py para funcionar com Django 1.0.

--- sigi/urls.py
+++ sigi/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^sigi/', include('sigi.casas.urls')),
-
-    # Uncomment this for admin:
-    (r'^admin/', include('django.contrib.admin.urls')),
+    (r'^admin/(.*)', admin.site.root),
 )
