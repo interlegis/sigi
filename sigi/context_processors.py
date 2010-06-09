8626bea3166f00feec03fa94251a8ba73ce519f9
Starlone (Estagiario) <starlone>
2010-06-09 20:59:10 +0000

    Alterando grafico de regioes do dashboard e grafico de convenios.

--- sigi/context_processors.py
+++ sigi/context_processors.py
@@ -1,5 +1,6 @@
 from sigi.apps.casas.models import CasaLegislativa
 from sigi.apps.convenios.models import Convenio, Projeto
+from sigi.apps.contatos.models import UnidadeFederativa
 
 def charts_data(request):
     casas = CasaLegislativa.objects.all()
@@ -10,7 +11,7 @@ def charts_data(request):
 
     num_convenios_firmados = convenios_firmados.count()
     num_convenios_nao_firmados = convenios.filter(data_retorno_assinatura=None).count()
-    num_casas_nao_aderidas = CasaLegislativa.objects.filter(convenio=None).count()
+    #num_casas_nao_aderidas = CasaLegislativa.objects.filter(convenio=None).count()
     #num_casas_nao_aderidas = casas.count() - convenios.exclude(data_adesao=None).count()
 
     # Verifica quantidade de convenios por projeto
@@ -25,13 +26,33 @@ def charts_data(request):
         casas.filter(municipio__uf__regiao='SD').count(),
         casas.filter(municipio__uf__regiao='SL').count()
     ]
-    num_convenios_firmados_regiao = [
-        convenios_firmados.filter(casa_legislativa__municipio__uf__regiao='CO').count(),
-        convenios_firmados.filter(casa_legislativa__municipio__uf__regiao='NO').count(),
-        convenios_firmados.filter(casa_legislativa__municipio__uf__regiao='NE').count(),
-        convenios_firmados.filter(casa_legislativa__municipio__uf__regiao='SD').count(),
-        convenios_firmados.filter(casa_legislativa__municipio__uf__regiao='SL').count()
-    ]
+    #num_convenios_firmados_regiao = [
+    #    convenios_firmados.filter(casa_legislativa__municipio__uf__regiao='CO').count(),
+    #    convenios_firmados.filter(casa_legislativa__municipio__uf__regiao='NO').count(),
+    #    convenios_firmados.filter(casa_legislativa__municipio__uf__regiao='NE').count(),
+    #    convenios_firmados.filter(casa_legislativa__municipio__uf__regiao='SD').count(),
+    #    convenios_firmados.filter(casa_legislativa__municipio__uf__regiao='SL').count()
+    #]
+    
+    REGIAO_CHOICES = ('CO','NO','NE','SD','SL')
+ 
+    # Busca numero de casas conveniadas por regiao
+    num_casas_conveniadas_regiao = []
+    for regiao in REGIAO_CHOICES:
+        num_casas_conveniadas_regiao.append(
+            CasaLegislativa.objects.filter(
+               convenio__casa_legislativa__municipio__uf__regiao=regiao
+            ).exclude(
+               convenio__data_retorno_assinatura=None
+            ).distinct().count()         
+        )
+
+    # Busca numero de casas sem convenio por regiao
+    num_casas_sem_convenio_regiao = []
+    for i in range(len(num_casas_regiao)):
+        num_casas_sem_convenio_regiao.append(
+            num_casas_regiao[i] - num_casas_conveniadas_regiao[i] 
+        )
     
     # Verifica qual regiao tem mais convenios e guarda valor para "axis left" do grafico de regioes
     num_regiao_maior = 0
@@ -43,11 +64,9 @@ def charts_data(request):
     equip_recebidos = convenios.exclude(data_termo_aceite=None).count()
 
     return {
-        'regioes_chart_data': [num_convenios_firmados_regiao, num_casas_regiao],
-        'convenios_chart_data': [num_convenios_firmados, num_convenios_nao_firmados,
-                                 num_casas_nao_aderidas],
+        'regioes_chart_data': [num_casas_conveniadas_regiao, num_casas_sem_convenio_regiao, num_regiao_maior],
+        'convenios_chart_data': [num_convenios_firmados, num_convenios_nao_firmados,],
         'equipamentos_chart_data': [equip_recebidos, equip_n_recebidos],
         'projetos_chart_data': projetos,
-        'convenios_por_projeto_chart_data': convenios_por_projeto,
-        'num_regiao_maior_chart_data': num_regiao_maior,
+        'convenios_por_projeto_chart_data': convenios_por_projeto,        
     }
