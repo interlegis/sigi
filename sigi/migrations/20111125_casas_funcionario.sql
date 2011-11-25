a11961f1db7d4c36d506d081abfae923f0b0ffbc
Felipe Vieira <felipe.tio@gmail.com>
2011-11-25 20:57:21 +0000

    adicionando migrations do modelo de casas_funcionario

---
+++ sigi/migrations/20111125_casas_funcionario.sql
@@ -0,0 +1,28 @@
+BEGIN;
+
+-- criando tabela do model Funcionario e Presidente
+CREATE TABLE "casas_funcionario" (
+    "id" serial NOT NULL PRIMARY KEY,
+    "casa_legislativa_id" integer NOT NULL REFERENCES "casas_casalegislativa" ("id") DEFERRABLE INITIALLY DEFERRED,
+    "nome" varchar(100) NOT NULL,
+    "nota" varchar(70) NOT NULL,
+    "email" varchar(75) NOT NULL,
+    "cargo" varchar(100),
+    "funcao" varchar(100),
+    "tempo_de_servico" varchar(50)
+);
+CREATE INDEX "casas_funcionario_casa_legislativa_id" ON "casas_funcionario" ("casa_legislativa_id");
+
+-- migrando dados de presidente da CasaLegislativa para Funcionarios
+INSERT INTO casas_funcionario (casa_legislativa_id, cargo, nome, nota, email)
+  SELECT id, 'Presidente', presidente, '', '' FROM casas_casalegislativa;
+
+-- migrando dados de telefones da CasaLegislativa para model generic Telefone
+INSERT INTO contatos_telefone (numero, tipo, content_type_id, object_id, codigo_area, nota)
+  SELECT telefone, 'F', 12, id, '', '' FROM casas_casalegislativa;
+
+-- deletando colunas da CasaLegislativa
+ALTER TABLE "casas_casalegislativa" DROP COLUMN presidente;
+ALTER TABLE "casas_casalegislativa" DROP COLUMN telefone;
+
+COMMIT;
