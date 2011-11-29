3e94b2ca1775ce1390af7f139bbb35c38023b8af
Felipe Vieira <felipe.tio@gmail.com>
2011-11-29 17:37:41 +0000

    resolvendo erro de carregamento dos formularios (desabilitando ajax do jquerymobile)

--- media/js/diagnosticos/diagnosticos_categoria_form.js
+++ media/js/diagnosticos/diagnosticos_categoria_form.js
@@ -1,36 +1,36 @@
-// cntabiliza a quantidade de requests
-// ajax para nao desabilitar o loader
-// antes da hora
-var nun_ajax = 0;
-
-// variaveis globais para as requisicoes
-// ajax
-$.ajaxSetup({
-  url: $(location).attr('href'),
-  cache: false,
-  type: 'POST',
-  dataType: "text",
-  beforeSend: function() {
-    nun_ajax++
-    $.mobile.showPageLoadingMsg();
-    $.mobile.fixedToolbars.show()
-  },
-  success: function() {
-    nun_ajax--
-    if (nun_ajax == 0) {
-      $.mobile.hidePageLoadingMsg();
+$('#page').live('pageinit', function(event){
+  // cntabiliza a quantidade de requests
+  // ajax para nao desabilitar o loader
+  // antes da hora
+  var nun_ajax = 0;
+  
+  // variaveis globais para as requisicoes
+  // ajax
+  $.ajaxSetup({
+    url: $(location).attr('href'),
+    cache: false,
+    type: 'POST',
+    dataType: "text",
+    beforeSend: function() {
+      nun_ajax++
+      $.mobile.showPageLoadingMsg();
       $.mobile.fixedToolbars.show()
+    },
+    success: function() {
+      nun_ajax--
+      if (nun_ajax == 0) {
+        $.mobile.hidePageLoadingMsg();
+        $.mobile.fixedToolbars.show()
+      }
+    },
+    error: function(msg) {
+      nun_ajax--
+      $.mobile.hidePageLoadingMsg();
+      $.mobile.fixedToolbars.show();
+      $("#open-dialog").click()
     }
-  },
-  error: function(msg) {
-    nun_ajax--
-    $.mobile.hidePageLoadingMsg();
-    $.mobile.fixedToolbars.show();
-    $("#open-dialog").click()
-  }
-});
+  });
 
-$( '#page').live('pageinit', function(event){
   // remove a resposta vazia da interface
   $("div.ui-radio span.ui-btn-text:contains('---------')").parentsUntil("ul").hide()
 
