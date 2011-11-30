2a71765624ad59374915729728a7e6733af978ef
Felipe Vieira <felipe.tio@gmail.com>
2011-11-30 22:02:06 +0000

    retirando feedback excessivo

--- media/js/diagnosticos/diagnosticos_categorias_form.js
+++ media/js/diagnosticos/diagnosticos_categorias_form.js
@@ -1,32 +1,12 @@
 $('#page').live('pageinit', function(event){
-  // cntabiliza a quantidade de requests
-  // ajax para nao desabilitar o loader
-  // antes da hora
-  var nun_ajax = 0;
-  
-  // variaveis globais para as requisicoes
-  // ajax
+  // variaveis globais para as requisicoes ajax
   $.ajaxSetup({
     url: $(location).attr('href'),
     cache: false,
     type: 'POST',
-    dataType: "text",
-    beforeSend: function() {
-      nun_ajax++
-      $.mobile.showPageLoadingMsg();
-      $.mobile.fixedToolbars.hide()
-    },
     success: function() {
-      nun_ajax--
-      if (nun_ajax == 0) {
-        $.mobile.hidePageLoadingMsg();
-        $.mobile.fixedToolbars.show()
-      }
     },
     error: function(msg) {
-      nun_ajax--
-      $.mobile.hidePageLoadingMsg();
-      $.mobile.fixedToolbars.show();
       $("#open-dialog").click()
     }
   });
