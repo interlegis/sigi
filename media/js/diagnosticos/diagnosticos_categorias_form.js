afd96de63b29bbb721610a320ef087986c49a1e0
Felipe Vieira <felipe.tio@gmail.com>
2011-11-30 18:37:37 +0000

    salvando dados em campos select tambem

--- media/js/diagnosticos/diagnosticos_categorias_form.js
+++ media/js/diagnosticos/diagnosticos_categorias_form.js
@@ -36,7 +36,7 @@ $('#page').live('pageinit', function(event){
 
   // para todo input do from registra um evento
   // ao modificar o campo
-  $("div.ui-field-contain input").change(function () {
+  $("div.ui-field-contain input, div.ui-field-contain select").change(function () {
     $.ajax({
       data: $('#diagnostico').serializeArray()
     })
