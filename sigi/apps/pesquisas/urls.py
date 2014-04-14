55e6daa9538def6bf73bf6f7ca896c966a45b086
Breno Teixeira <brenotx@gmail.com>
2014-04-14 16:22:31 -0300

    Correção de link para as views

--- sigi/apps/pesquisas/urls.py
+++ sigi/apps/pesquisas/urls.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.conf.urls import patterns, url
 
 
-urlpatterns = patterns("apps.pesquisas.views",
+urlpatterns = patterns("sigi.apps.pesquisas.views",
     url(r"(?P<slug>.*)/sent/$", "form_sent", name="form_sent"),
     url(r"(?P<slug>.*)/$", "form_detail", name="form_detail"),
 )
