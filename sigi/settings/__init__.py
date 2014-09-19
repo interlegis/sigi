207cce213fa84b0bdd5558eef52d1d884982a8c5
Marcio Mazza <marciomazza@gmail.com>
2014-09-19 13:33:37 -0300

    Exigencia explicita do settings de producao (prod.py)

--- sigi/settings/__init__.py
+++ sigi/settings/__init__.py
@@ -1,7 +1,13 @@
 try:
-    from .prod import *
+    from prod import *
 except ImportError:
-    from warnings import warn
-    msg = "You don't have a production settings file, using development settings."
-    warn(msg, category=ImportWarning)
-    from .dev import *
+    from django.core.exceptions import ImproperlyConfigured
+    msg = """
+
+  ######################################################################
+  Arquivo prod.py (django settings) nao encontrado.
+  Se vc esta num ambiente de desenvolvimento pode cria-lo com
+    ln -s dev.py prod.py
+  ######################################################################
+"""
+    raise ImproperlyConfigured(msg)
