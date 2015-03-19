fd9e7aa67ef815163bc3e1353ad3251fe28abdef
Marcio Mazza <mazza@interlegis.leg.br>
2015-03-19 15:20:03 -0300

    Add ad-hoc script to grap parliaments that use PM and SAPL

---
+++ scripts/casas_com_PM_e_SAPL.py
@@ -0,0 +1,35 @@
+from csv_writer import CsvWriter
+from sigi.apps.servicos.models import Servico
+
+
+ARQUIVO_CSV = '/tmp/casas_que_usam_PM_e_SAPL.csv'
+
+
+def escrever_casas(casas, arquivo):
+    with open(arquivo, "wb") as f:
+        writer = CsvWriter(f)
+        writer.writerow([
+            "casa: sigla UF", "casa: nome UF", "casa: NOME",
+        ])
+        for casa in casas:
+            writer.writerow([
+                casa.municipio.uf.sigla, casa.municipio.uf, casa.nome,
+            ])
+
+casas_pm = {s.casa_legislativa for s in Servico.objects.filter(tipo_servico__sigla='PM', data_desativacao__isnull=True)}
+casas_sapl = {s.casa_legislativa for s in Servico.objects.filter(tipo_servico__sigla='SAPL', data_desativacao__isnull=True)}
+casas_ativas = casas_pm & casas_sapl
+escrever_casas(casas_ativas, '/tmp/casas_que_usam_PM_e_SAPL__AMBOS_ATIVOS.csv')
+
+casas_pm = {s.casa_legislativa for s in Servico.objects.filter(tipo_servico__sigla='PM')}
+casas_sapl = {s.casa_legislativa for s in Servico.objects.filter(tipo_servico__sigla='SAPL')}
+casas_todas = casas_pm & casas_sapl
+escrever_casas(casas_todas, '/tmp/casas_que_usam_PM_e_SAPL__TODOS.csv')
+
+
+casas = casas_pm.union(casas_sapl)
+escrever_casas(casas, '/tmp/casas_que_usam_PM_ou_SAPL.csv')
+
+
+casas_algum_inativo = casas_todas
+
---
+++ scripts/csv_writer.py
@@ -0,0 +1,38 @@
+import cStringIO
+import codecs
+import csv
+
+
+class CsvWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+
+    from https://docs.python.org/2/library/csv.html
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8"):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def clean(self, cell):
+        return unicode(cell) if cell else '-'
+
+    def writerow(self, row):
+        self.writer.writerow([self.clean(s).encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
