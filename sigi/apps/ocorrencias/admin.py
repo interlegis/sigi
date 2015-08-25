72ec48f13c2a0840ae505dc36db874d66ea49169
Sesostris Vieira <sesostris@interlegis.leg.br>
2015-08-25 12:32:30 -0300

    Add descricao and resolucao fields to Ocorrencias admin search_fields list

--- sigi/apps/ocorrencias/admin.py
+++ sigi/apps/ocorrencias/admin.py
@@ -65,7 +65,7 @@ class OcorrenciaChangeList(ChangeList):
 class OcorrenciaAdmin(BaseModelAdmin):
     list_display = ('data_criacao', 'casa_legislativa', 'get_municipio', 'get_uf', 'assunto', 'prioridade', 'status', 'data_modificacao', 'setor_responsavel',)
     list_filter = (OcorrenciaListFilter, 'status', 'prioridade', 'categoria__nome', 'setor_responsavel__nome', 'casa_legislativa__gerente_contas',)
-    search_fields = ('casa_legislativa__search_text', 'assunto', 'servidor_registro__nome_completo', )
+    search_fields = ('casa_legislativa__search_text', 'assunto', 'servidor_registro__nome_completo', 'descricao', 'resolucao',)
     date_hierarchy = 'data_criacao'
     fields = ('casa_legislativa', 'categoria', 'tipo_contato', 'assunto', 'status', 'prioridade', 'descricao', 'servidor_registro',
               'setor_responsavel', 'resolucao', )
