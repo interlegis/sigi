0d8e690600abc8ced8a5644fa63bbdea4b2ce7e3
Sesostris Vieira <sesostris@interlegis.leg.br>
2015-11-05 11:59:31 -0200

    Fix url error for Convenio report by region

--- sigi/apps/convenios/urls.py
+++ sigi/apps/convenios/urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns(
     url(r'^convenio/carrinho/excluir_carrinho/$', 'excluir_carrinho', name='excluir-carrinho'),  # tagerror
     url(r'^convenio/carrinho/deleta_itens_carrinho$', 'deleta_itens_carrinho', name='deleta-itens-carrinho'),  # tagerror
     url(r'^convenio/csv/$', 'export_csv', name='convenios-csv'),
-    url(r'^convenio/reportsRegiao/(?P<regiao>\w+)/$', 'report_regiao', name='convenios-report_regiao_pdf'),
+    url(r'^reportsRegiao/(?P<regiao>\w+)/$', 'report_regiao', name='convenios-report_regiao_pdf'),
 )
