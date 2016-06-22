17d5ae8d5f500b021ea28fb3d24bad25d43645a7
Eduardo Edson Batista Cordeiro Alves <eduardoalves@interlegis.leg.br>
2016-06-22 15:05:51 -0300

    Adiciona arquivo css separado para atendimento

--- sigi/settings/base.py
+++ sigi/settings/base.py
@@ -194,10 +194,6 @@ LOGGING = {
     },
 }
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "collected_static")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, ("static")),)
-
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
