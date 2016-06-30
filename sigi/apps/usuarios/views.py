d744af9fdd3ff074bb59017f996a6ce7421d3faa
Eduardo Edson Batista Cordeiro Alves <eduardoalves@interlegis.leg.br>
2016-06-30 17:35:00 -0300

    Mensagem de perfil habilitado/não habilitado

--- sigi/apps/usuarios/views.py
+++ sigi/apps/usuarios/views.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
-from braces.views import GroupRequiredMixin
 
 from braces.views import FormValidMessageMixin, GroupRequiredMixin
 from django.conf import settings
