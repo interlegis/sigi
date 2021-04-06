c83382173eb474822594ab95d46ac1fb8a841d94
Sesóstris Vieira <sesostris@interlegis.leg.br>
2021-04-06 16:21:49 -0300

    Alteração verbose_name de TipoOrgao

---
+++ sigi/apps/casas/migrations/0012_auto_20210406_1420.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('casas', '0011_auto_20210406_1135'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='tipoorgao',
+            options={},
+        ),
+    ]
