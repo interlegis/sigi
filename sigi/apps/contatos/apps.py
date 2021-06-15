e79a20240327155ba614ee846d6b45dd98ebef73
Sesóstris Vieira <sesostris@interlegis.leg.br>
2021-06-14 22:55:59 -0300

    Preparando migração para django 3

---
+++ sigi/apps/contatos/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class UtilsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sigi.apps.contatos'
+    label = 'contatos'
+    verbose_name = 'Contatos'
