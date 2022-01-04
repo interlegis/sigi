16e206151314217cc433d2e266697b1866afb655
Sesóstris Vieira <sesostris@interlegis.leg.br>
2022-01-04 14:51:41 -0300

    Preparando para Django 4

---
+++ sigi/settings/development.py
@@ -0,0 +1,45 @@
+from sigi.settings.base import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-z%cw$-jyh13g2an=8r5ltkyub75bfc$34o(k#%jncdcdgr2$3#'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sigi',
+        'USER': 'sigi',
+        'PASSWORD': '123456',
+        'HOST': 'database',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
--- sigi/urls.py
+++ sigi/urls.py
@@ -1,42 +1,21 @@
-#-*- coding:utf-8 -*-
-from django.conf import settings
-from django.conf.urls import patterns, include, url
-from django.conf.urls.static import static
+"""sigi URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.views.generic.base import TemplateView
+from django.urls import path
 
-admin.site.index_template = 'index.html'
-admin.autodiscover()
-
-urlpatterns = patterns(
-    '',
-
-    url(r'^parlamentares/', include('sigi.apps.parlamentares.urls')),
-    url(r'^casas/', include('sigi.apps.casas.urls')),
-    url(r'^convenios/', include('sigi.apps.convenios.urls')),
-    url(r'^diagnosticos/', include('sigi.apps.diagnosticos.urls')),
-    url(r'^servicos/', include('sigi.apps.servicos.urls')),
-    url(r'^dashboard/', include('sigi.apps.metas.urls')),
-    url(r'^ocorrencias/', include('sigi.apps.ocorrencias.urls')),
-    url(r'^eventos/', include('sigi.apps.eventos.urls')),
-    url(r'^whois/', include('sigi.apps.whois.urls')),
-    url(r'^tinymce/', include('tinymce.urls')),
-    url(r'^', include('sigi.apps.home.urls')),
-    url(r'^', include(admin.site.urls)),
-
-    # to enable language selection
-    # Suspended
-    #url(r'^i18n/', include('django.conf.urls.i18n')),
-
-) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-if settings.DEBUG:
-    urlpatterns = patterns(
-        '',
-
-        url(r'^404/$', TemplateView.as_view(template_name='404.html')),
-        url(r'^500/$', TemplateView.as_view(template_name='500.html')),
-        url(r'^503/$', TemplateView.as_view(template_name='503.html')),
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.MEDIA_ROOT, }),
-    ) + urlpatterns
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
