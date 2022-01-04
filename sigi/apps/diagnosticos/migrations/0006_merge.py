1039881b4f4cf0295e58d0aa53a329fe8f4f1d57
Sesóstris Vieira <sesostris@interlegis.leg.br>
2022-01-04 12:48:51 -0300

    Resolve conflitos de migração

---
+++ sigi/apps/diagnosticos/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('diagnosticos', '0002_auto_20160719_0920'),
+        ('diagnosticos', '0005_auto_20210416_0841'),
+    ]
+
+    operations = [
+    ]
