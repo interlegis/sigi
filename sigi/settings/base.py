3be78128459aeb96f012ec683bbc8e1756589a42
Sesóstris Vieira <sesostris@interlegis.leg.br>
2022-01-21 00:08:21 -0300

    Adiciona tema Django-material-admin

--- sigi/settings/base.py
+++ sigi/settings/base.py
@@ -35,8 +35,8 @@ INSTALLED_APPS = [
     'tinymce',
     'django_bootstrap5',
     'django.forms',
-    # 'material',
-    # 'material.admin',
+    'material',
+    'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -114,26 +114,26 @@ IMPORT_EXPORT_USE_TRANSACTIONS = True
 IMPORT_EXPORT_SKIP_ADMIN_LOG = True
 
 # Django Material Admin settings
-#
+# https://github.com/MaistrenkoAnton/django-material-admin#django-material-administration
 
-# MATERIAL_ADMIN_SITE = {
-#     'HEADER':  _('SIGI'),
-#     'TITLE':  _('SIGI - Sistema de Informações do Interlegis'),
-#     'FAVICON':  'img/favicon.ico',
-#     # 'MAIN_BG_COLOR':  'color',  # Admin site main color, css color should be specified
-#     # 'MAIN_HOVER_COLOR':  'color',  # Admin site main hover color, css color should be specified
-#     'PROFILE_PICTURE':  'img/interlegis.jpeg',  # Admin site profile picture (path to static should be specified)
-#     'PROFILE_BG':  'img/engitec.jpeg',  # Admin site profile background (path to static should be specified)
-#     'LOGIN_LOGO':  'img/interlegis.jpeg',  # Admin site logo on login page (path to static should be specified)
-#     'LOGOUT_BG':  'img/engitec.jpeg',  # Admin site background on login/logout pages (path to static should be specified)
-#     'SHOW_THEMES':  True,  #  Show default admin themes button
-#     'TRAY_REVERSE': True,  # Hide object-tools and additional-submit-line by default
-#     'NAVBAR_REVERSE': True,  # Hide side navbar by default
-#     # 'SHOW_COUNTS': True, # Show instances counts for each model
-#     # 'APP_ICONS': {  # Set icons for applications(lowercase), including 3rd party apps, {'application_name': 'material_icon_name', ...}
-#     #     'sites': 'send',
-#     # },
-#     # 'MODEL_ICONS': {  # Set icons for models(lowercase), including 3rd party models, {'model_name': 'material_icon_name', ...}
-#     #     'site': 'contact_mail',
-#     # }
-# }
+MATERIAL_ADMIN_SITE = {
+    'HEADER':  _('SIGI'),
+    'TITLE':  _('SIGI - Sistema de Informações do Interlegis'),
+    'FAVICON':  'img/favicon.ico',
+    # 'MAIN_BG_COLOR':  'color',  # Admin site main color, css color should be specified
+    # 'MAIN_HOVER_COLOR':  'color',  # Admin site main hover color, css color should be specified
+    'PROFILE_PICTURE':  'img/interlegis.jpeg',  # Admin site profile picture (path to static should be specified)
+    'PROFILE_BG':  'img/engitec.jpeg',  # Admin site profile background (path to static should be specified)
+    'LOGIN_LOGO':  'img/interlegis.jpeg',  # Admin site logo on login page (path to static should be specified)
+    'LOGOUT_BG':  'img/engitec.jpeg',  # Admin site background on login/logout pages (path to static should be specified)
+    'SHOW_THEMES':  True,  #  Show default admin themes button
+    'TRAY_REVERSE': True,  # Hide object-tools and additional-submit-line by default
+    'NAVBAR_REVERSE': True,  # Hide side navbar by default
+    # 'SHOW_COUNTS': True, # Show instances counts for each model
+    # 'APP_ICONS': {  # Set icons for applications(lowercase), including 3rd party apps, {'application_name': 'material_icon_name', ...}
+    #     'sites': 'send',
+    # },
+    # 'MODEL_ICONS': {  # Set icons for models(lowercase), including 3rd party models, {'model_name': 'material_icon_name', ...}
+    #     'site': 'contact_mail',
+    # }
+}
