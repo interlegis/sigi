13d90ec9666b7b4a09d17a48f3229953bde04553
Sesóstris Vieira <sesostris@interlegis.leg.br>
2022-04-14 11:25:38 -0300

    Ajuste na migração de dados

--- sigi/apps/casas/migrations/0014_auto_20210406_1945.py
+++ sigi/apps/casas/migrations/0014_auto_20210406_1945.py
@@ -7,17 +7,13 @@ import sigi.apps.utils
 class Migration(migrations.Migration):
 
     dependencies = [
-        # ('metas', '0002_auto_20210406_1945'),
         ('contatos', '0002_auto_20151104_0810'),
         ('servidores', '0001_initial'),
-        # ('parlamentares', '0002_auto_20210406_1945'),
-        # ('servicos', '0005_auto_20210406_1945'),
-        # ('servicos', '0004_delete_casaatendida'),
-        # ('inventario', '0002_auto_20210406_1945'),
-        # ('convenios', '0003_auto_20210406_1945'),
-        # ('ocorrencias', '0003_auto_20210406_1945'),
-        # ('diagnosticos', '0004_auto_20210406_1945'),
-        # ('eventos', '0005_auto_20210406_1945'),
+        ('servicos', '0004_delete_casaatendida'),
+        ('inventario', '0001_initial'),
+        ('convenios', '0002_convenio_duracao'),
+        ('ocorrencias', '0002_auto_20160308_0828'),
+        ('eventos', '0004_remove_evento_curso_moodle_id'),
         ('casas', '0013_auto_20210406_1428'),
     ]
 
